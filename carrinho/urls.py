3cfc96fb20c50dc0a9a0fba0356149be4bde65df
ianottoniel <jobian3@outlook.com>
2025-06-05 20:38:26 -0300

    ian

--- carrinho/urls.py
+++ carrinho/urls.py
@@ -4,11 +4,12 @@ from . import views
 app_name = 'carrinho'
 
 urlpatterns = [
+    # Rotas principais do carrinho
     path('', views.detalhe_carrinho, name='detalhe'),
     path('adicionar/<int:produto_id>/', views.adicionar_carrinho, name='adicionar'),
     path('remover/<int:produto_id>/', views.remover_carrinho, name='remover'),
     path('limpar/', views.limpar_carrinho, name='limpar'),
-    
-    # URLs AJAX (opcionais)
+
+    # Rotas para requisições AJAX
     path('ajax/adicionar/<int:produto_id>/', views.adicionar_carrinho_ajax, name='adicionar_ajax'),
 ]
